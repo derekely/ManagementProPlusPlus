318778cb69eaec95ef880e4d5b7ae8152f6736c8
wadewithers <wit20007@byui.edu>
2023-03-06 09:02:41 -0700

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 // import  ReactDOM from "react";
 import Card from './Card';
+import NavBar  from "./components/NavBar";
 
 function App() {
   return(
     <>
+    <NavBar />
     <Card />
     <button>Add New Task</button>
     </>
---
+++ src/components/NavBar.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import "./NavBarStyles.css";
+
+class NavBar extends Component {
+
+    state = { clicked: false };
+    // optional change state of navbar when clicked if in small screen
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
+
+    render() {
+    return (
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                {/* add link to logo in href */}
+                <a className="navbar-brand" href="index.html">add logo here</a>
+                {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button> */}
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0" id="navbar">
+                        <li className="nav-item">
+                            {/* add link to home page in href */}
+                            <a className="nav-link active" aria-current="page" href="index.html">
+                                Home
+                            </a>
+                        </li>
+                        {/* add link to projects page in href */}
+                        <li className="nav-item dropdown">
+                            <a className="nav-link dropdown-toggle" href="index.html" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                Projects
+                            </a>
+                            {/* Optional dropdown menu */}
+                            {/* <ul className="dropdown-menu">
+                                <li><a className="dropdown-item" href="index.html">Project 1</a></li>
+                                <li><a className="dropdown-item" href="index.html">Project 2</a></li>
+                                <li><a className="dropdown-item" href="index.html">Project 3</a></li>
+                            </ul> */}
+                        </li>
+                        {/* add link to login page in href */}
+                        <li className="nav-item">
+                            <a className="nav-link active" href="index.html">
+                                Login
+                            </a>
+                        </li> 
+                    </ul>
+                    {/* Optional search menu */}
+                    {/* <form className="d-flex" role="search">
+                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+                        <button className="btn btn-outline-success" type="submit">Search</button>
+                    </form> */}
+                </div>
+            </div>
+            {/* Optional change navbar to fit small screen */}
+            <div id="mobile" onClick={this.handleClick}>
+                <i
+                    id="bar"
+                    className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
+                ></i>
+
+            </div>
+        </nav>
+    );
+}
+}
+
+export default NavBar;
