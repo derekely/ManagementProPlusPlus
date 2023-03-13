5331e41eae212917ae4e87c6d26d6db56be70c68
Brandon Schliep <sch16104@byui.edu>
2023-03-13 08:22:41 -0600

    Added Card style

--- src/Card.js
+++ src/Card.js
@@ -2,28 +2,18 @@ import React from "react";
 
 // When calling this use the card function 
 // pass through something like example
-// <Card
-//    title="Title"
-//    cardDescription="short description of project"
-//    cardReference="the referenced page for the card">
 export default function Card(){
     return (
-        <div className="card">
-            <div className="cardBody">
-                <h2 className="cardTitle">Card</h2>
-                <p className="cardDescription">Card Description</p>
-                <p className="cardDate">Card Date</p>
+        <Card style={{ width: '18rem' }}>
+            <div className="card">
+                <div className="cardBody">
+                    <h2 className="cardTitle">Card</h2>
+                    <p className="cardDescription">Card Description</p>
+                    <p className="cardDate">Card Date</p>
+                </div>
             </div>
-        </div>
+        </Card>
     )
-    // <div className="card">
-    //     <div className="cardBody">
-    //         <h2 className="cardTitle">{props.cardTitle}</h2>
-    //         <p className="cardDescription">{props.cardDescription}</p>
-    //         <button className="cardButton" href={props.cardReference}>View Card</button>
-    //         <p className="cardDate">{props.date}</p>
-    //     </div>
-    // </div>
 }
 
 
