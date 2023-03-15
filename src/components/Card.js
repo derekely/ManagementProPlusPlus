36dfacaa569c23c9a3823549b5a14f4c908572c2
Brandon Schliep <sch16104@byui.edu>
2023-03-15 08:59:19 -0600

    Added the height and width to the card.

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,8 +6,8 @@ export default function Card(){
         <div className="card">
             <div className="cardBody">
                 <h2 className="cardTitle">Card</h2>
-                <p className="cardDescription">Card Description</p>
                 <p className="cardDate">Card Date</p>
+                <p className="cardDescription">Card Description</p>
             </div>
         </div>      
     )
