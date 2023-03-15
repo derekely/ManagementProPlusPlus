713c6be59e0229c81aac00b81454ac43dcd8677c
Derek Ely <derekely21@gmail.com>
2023-03-15 08:45:03 -0600

    added AddCard()

--- src/Card.js
+++ src/Card.js
@@ -2,6 +2,8 @@ import React from "react";
 
 // When calling this use the card function 
 // pass through something like example
+let projects = [];
+
 export default function Card(){
     return (
         <Card style={{ width: '18rem' }}>
@@ -19,3 +21,23 @@ export default function Card(){
 
 
 // An idea for this would make a popup asking for each of the card's details
+// Makes a window -- or adds JSX -- after button press to create new project
+export function AddCard(){
+    return(
+        <>
+            <label>
+                Project Title <input name="cardTitle" />
+            </label>
+            <hr />
+            <label>
+                Project Description <input name="cardDescription" />
+            </label>
+            <hr />
+            <label>
+                Project Start Date <input name="cardDate" />
+            </label>
+            <hr />
+            <button>Add Project to tasks!</button>
+        </>
+    )
+}
