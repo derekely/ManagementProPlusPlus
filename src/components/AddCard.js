088d9adfce7da0f226675add9c47135e2849f9d3
Derek Ely <derekely21@gmail.com>
2023-03-19 17:39:33 -0600

    got popup to open and close

--- src/components/AddCard.js
+++ src/components/AddCard.js
@@ -1,23 +1,28 @@
-import React from "react";
-
+import React, { useState } from "react";
+import "./AddCard.css";
 // Makes a window -- or adds JSX -- after button press to create new project
-export default function AddCard(){
+export default function AddCard(props){
+ 
     
-    return(
-        <>
-            <label>
-                Project Title <input name="cardTitle" />
-            </label>
-            <hr />
-            <label>
-                Project Description <input name="cardDescription" />
-            </label>
-            <hr />
-            <label>
-                Project Start Date <input name="cardDate" />
-            </label>
-            <hr />
-            <button>Add Project to tasks!</button>
-        </>
-    )
+    return(props.trigger) ? (
+        <div className="popup">
+            <div className="popup-inner">
+                <label>
+                    Project Title <input name="cardTitle" />
+                </label>
+                <hr />
+                <label>
+                    Project Description <input name="cardDescription" />
+                </label>
+                <hr />
+                <label>
+                    Project Start Date <input name="cardDate" />
+                </label>
+                <hr />
+                <button className="close-btn" onClick={()=> props.setTrigger(false)}>x</button>
+                <button className="add-to-task-btn">Add Project to tasks!</button>
+                { props.children }
+            </div>
+        </div>
+    ) : ""
 }
