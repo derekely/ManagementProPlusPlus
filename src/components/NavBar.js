fc286d568bcddbabb89d7652d4080b84b2920cd2
wadewithers <wit20007@byui.edu>
2023-03-20 08:16:14 -0600

    Updates Navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,68 +1,36 @@
-import React, { Component } from "react";
+import React from "react";
 import "./NavBarStyles.css";
-
-class NavBar extends Component {
-
-    state = { clicked: false };
-    // optional change state of navbar when clicked if in small screen
-    handleClick = () => {
-        this.setState({clicked: !this.state.clicked})
-    }
-
-    render() {
+//logo.png
+function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                {/* add link to logo in href */}
-                <a className="navbar-brand" href="index.html">add logo here</a>
-                {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button> */}
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0" id="navbar">
-                        <li className="nav-item">
-                            {/* add link to home page in href */}
-                            <a className="nav-link active" aria-current="page" href="index.html">
-                                Home
-                            </a>
-                        </li>
-                        {/* add link to projects page in href */}
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="index.html" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Projects
-                            </a>
-                            {/* Optional dropdown menu */}
-                            {/* <ul className="dropdown-menu">
-                                <li><a className="dropdown-item" href="index.html">Project 1</a></li>
-                                <li><a className="dropdown-item" href="index.html">Project 2</a></li>
-                                <li><a className="dropdown-item" href="index.html">Project 3</a></li>
-                            </ul> */}
-                        </li>
-                        {/* add link to login page in href */}
-                        <li className="nav-item">
-                            <a className="nav-link active" href="index.html">
-                                Login
-                            </a>
-                        </li> 
-                    </ul>
-                    {/* Optional search menu */}
-                    {/* <form className="d-flex" role="search">
-                        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                        <button className="btn btn-outline-success" type="submit">Search</button>
-                    </form> */}
-                </div>
+        <nav className="navbar">
+            {/* Logo on the left */}
+            <div className="navbar_logo">
+                {/* <img src="logo.png" alt="Logo" /> */}
+                <a>
+                <svg id="logo-85" width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg"><path class="ccustom" fill-rule="evenodd" clip-rule="evenodd" d="M10 0C15.5228 0 20 4.47715 20 10V0H30C35.5228 0 40 4.47715 40 10C40 15.5228 35.5228 20 30 20C35.5228 20 40 24.4772 40 30C40 32.7423 38.8961 35.2268 37.1085 37.0334L37.0711 37.0711L37.0379 37.1041C35.2309 38.8943 32.7446 40 30 40C27.2741 40 24.8029 38.9093 22.999 37.1405C22.9756 37.1175 22.9522 37.0943 22.9289 37.0711C22.907 37.0492 22.8852 37.0272 22.8635 37.0051C21.0924 35.2009 20 32.728 20 30C20 35.5228 15.5228 40 10 40C4.47715 40 0 35.5228 0 30V20H10C4.47715 20 0 15.5228 0 10C0 4.47715 4.47715 0 10 0ZM18 10C18 14.4183 14.4183 18 10 18V2C14.4183 2 18 5.58172 18 10ZM38 30C38 25.5817 34.4183 22 30 22C25.5817 22 22 25.5817 22 30H38ZM2 22V30C2 34.4183 5.58172 38 10 38C14.4183 38 18 34.4183 18 30V22H2ZM22 18V2L30 2C34.4183 2 38 5.58172 38 10C38 14.4183 34.4183 18 30 18H22Z" fill="#5417D7"></path></svg>
+                </a>
             </div>
-            {/* Optional change navbar to fit small screen */}
-            <div id="mobile" onClick={this.handleClick}>
-                <i
-                    id="bar"
-                    className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}
-                ></i>
-
+            
+            {/* Links to 3 different nav sites */}
+            <ul className="navbar_links">
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Projects</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+            </ul>
+            
+            {/* Logo for account info/log in on the right */}
+            <div className="navbar_account">
+                <img src="https://thumbs.dreamstime.com/z/user-icon-member-login-vector-isolated-white-background-form-155134186.jpg" alt="Account" />
             </div>
         </nav>
     );
-}
-}
+};
 
-export default NavBar;
+export default Navbar; 
