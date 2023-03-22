b1287757d84ea7291d79bfb3224e600df49b4329
Derek Ely <derekely21@gmail.com>
2023-03-22 09:44:12 -0600

    moved app.js from different branch to main brach

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 // import  ReactDOM from "react";
+import  AddCard  from "./components/AddCard";
 import Card from './components/Card';
 import NavBar  from "./components/NavBar";
 
 function App() {
+  const [buttonPopup, setButtonPopup] = useState(false)
   return(
     <>
     <NavBar />
     <Card /> 
-    <button>New</button>
+    <button onClick={() => setButtonPopup(true)}>+</button>
+    <AddCard trigger={buttonPopup} setTrigger={setButtonPopup}></AddCard>
     </>
   )
 }
