1f6af4855c5ee779542fd1e742faa8ca36e15ecb
Derek Ely <derekely21@gmail.com>
2023-03-23 21:23:24 -0600

    finnished the looks of the popup window.

--- src/components/AddCard.js
+++ src/components/AddCard.js
@@ -1,23 +1,49 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Card from "./Card";
+import "./AddCard.css";
 // Makes a window -- or adds JSX -- after button press to create new project
-export default function AddCard(){
+export default function AddCard(props){
     
-    return(
-        <>
-            <label>
-                Project Title <input name="cardTitle" />
-            </label>
-            <hr />
-            <label>
-                Project Description <input name="cardDescription" />
-            </label>
-            <hr />
-            <label>
-                Project Start Date <input name="cardDate" />
-            </label>
-            <hr />
-            <button>Add Project to tasks!</button>
-        </>
-    )
+    function handleAddTaskClick() {
+        let card = <Card {...props} />;
+        console.log(props);
+        props.setTrigger = false;
+        // Do something with the card variable, such as adding it to the DOM
+      }
+      
+    
+    return(props.trigger) ? (
+        <div className="popup">
+            <div className="popup-inner">
+                <button className="close-btn" onClick={()=> props.setTrigger(false)}>x</button>
+                    <h1>CREATE YOUR OWN PROJECT!</h1>
+                    <div id="top-row">
+                        <label className="project-Title">
+                            Project Title <input placeholder="My Project" name="cardTitle" />
+                        </label>
+                        
+                        <label className="project-Date">
+                            Project Due Date <input type="date" name="cardDate" />
+                        </label>
+                    </div>
+                    <div id="middle-row">
+                        <label htmlFor="project-description">
+                            Project Description
+                        </label>
+                        <textarea id="project-description" placeholder='"It"s the little details that are vital. Little things make big things happen."   -John Wooden, Basketball Coach"'></textarea>
+                    </div>
+                <div id="bottom-row">
+                    <button className="add-to-task-btn" onClick={handleAddTaskClick}>Add Project to tasks!</button>
+                    { props.children }
+                </div>
+                
+            </div>
+        </div>
+    ) : ""
+
+
+
+
+
+
 }
